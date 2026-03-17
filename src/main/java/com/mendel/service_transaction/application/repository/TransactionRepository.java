138f7938e4ca7878070b3f73f0dee10a864cbe9b
Niquitoo <nicolas.zajic@gmail.com>
2026-03-17 01:49:07 -0600

    ading application repository transaction

---
+++ src/main/java/com/mendel/service_transaction/application/repository/TransactionRepository.java
@@ -0,0 +1,13 @@
+package com.mendel.service_transaction.application.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.mendel.service_transaction.domain.model.Transaction;
+
+public interface TransactionRepository {
+	void save(Transaction transaction);
+	Optional<Transaction> findById(Long id);
+	List<Transaction> findByType(String type);
+	List<Transaction> findAll();
+}
